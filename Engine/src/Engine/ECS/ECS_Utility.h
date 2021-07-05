a93dd6a5f79cd918301236689868880c9ba23747
Lim Guan Hui <l.guanhui@digipen.edu>
2021-07-05 17:33:05 +0800

    put ECS utility into engine namespace

--- Engine/src/Engine/ECS/ECS_Utility.h
+++ Engine/src/Engine/ECS/ECS_Utility.h
@@ -17,9 +17,13 @@ Technology is prohibited.
 #include "pch.h"
 #include <bitset>
 
-using Entity = std::uint32_t;
-constexpr Entity MAX_ENTITY = 5000;
+namespace engine
+{
+	using Entity = std::uint32_t;
+	constexpr Entity MAX_ENTITY = 5000;
 
-using ComponentType = std::uint16_t;
-const ComponentType MAX_COMPONENTS = 128;
-using Signature = std::bitset<MAX_COMPONENTS>;
+	using ComponentType = std::uint16_t;
+	const ComponentType MAX_COMPONENTS = 128;
+	using Signature = std::bitset<MAX_COMPONENTS>;
+
+}
