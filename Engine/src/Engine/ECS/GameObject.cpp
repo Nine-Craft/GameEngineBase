0130a184adc72f495dad16f95cb8323430247603
Spironan <c_tecklee@hotmail.com>
2021-07-07 15:54:19 +0800

    GameObject Class with Scenegraph "node" support

---
+++ Engine/src/Engine/ECS/GameObject.cpp
@@ -0,0 +1,85 @@
+/************************************************************************************//*!
+\file           GameObject.cpp
+\project        INSERT PROJECT NAME
+\author         Chua Teck Lee, c.tecklee, 390008420
+\par            email: c.tecklee\@digipen.edu
+\date           Jul 07, 2021
+\brief          Describes a gameobject which is the interface for everything ECS related
+
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*//*************************************************************************************/
+#include "pch.h"
+#include "GameObject.h"
+
+#include "Engine/Transform/Transform3D.h"
+
+namespace engine
+{
+    GameObject::GameObject()
+    {
+        //m_entity = ECS_Manager::GenerateUID();
+        
+        //AddComponent<Transform3D>();
+    }
+
+    GameObject::~GameObject()
+    {
+        
+    }
+
+    void GameObject::AddChild(GameObject* gameObj, bool preserveTransforms)
+    {
+        // Flag coordinates to be converted when parented
+        if (preserveTransforms)
+        {
+            //gameObj->GetComponent<Transform3D>().ConvertCoordinates();
+        }
+
+        children.push_back(gameObj);
+    }
+
+    void GameObject::AddChild(std::initializer_list<GameObject*> gameObjs, bool preserveTransforms)
+    {
+        for (GameObject* obj : gameObjs)
+        {
+            AddChild(obj, preserveTransforms);
+        }
+    }
+
+    void GameObject::RemoveChild(GameObject* gameObj)
+    {
+        children.erase(std::find(children.begin(), children.end(), gameObj));
+    }
+
+    std::vector<GameObject*> const& GameObject::GetChildren() const
+    {
+        return children;
+    }
+
+    GameObject* GameObject::FindGameObjectInChildrenByName(std::string const& name)
+    {
+        // Is this the one?
+        if (Name == name)
+        {
+            return this;
+        }
+
+        // Search Children
+        for (auto& go : children)
+        {
+            // Checks to see if the object was found
+            GameObject* findResult = go->FindGameObjectInChildrenByName(name);
+            if (findResult)
+            {
+                // Found!
+                return findResult;
+            }
+        }
+
+        // Went through everything and didn't find anything
+        return nullptr;
+    }
+}
---
+++ Engine/src/Engine/ECS/GameObject.h
@@ -0,0 +1,173 @@
+/************************************************************************************//*!
+\file           GameObject.h
+\project        INSERT PROJECT NAME
+\author         Chua Teck Lee, c.tecklee, 390008420
+\par            email: c.tecklee\@digipen.edu
+\date           Jul 07, 2021
+\brief          Describes a gameobject which is the interface for everything ECS related
+
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*//*************************************************************************************/
+#pragma once
+
+#include "ECS.h"
+
+namespace engine
+{
+    class GameObject final
+    {
+    private:
+        Entity m_entity;
+        bool m_active;
+
+        std::vector<GameObject*> children;
+
+    public:
+        std::string Name;
+
+        GameObject();
+        ~GameObject();
+        
+        constexpr Entity GetID() const { return m_entity; }
+
+        constexpr bool IsActive() const { return m_active; }
+        void SetActive(bool const active) { m_active = active; }
+        
+        void AddChild(GameObject* gameObj, bool preserveTransforms = false);
+        void AddChild(std::initializer_list<GameObject*> gameObjs, bool preserveTransforms = false);
+        void RemoveChild(GameObject* gameObj);
+        std::vector<GameObject*> const& GetChildren() const;
+
+        GameObject* FindGameObjectInChildrenByName(std::string const& name);
+
+
+        //template<typename Component>
+        //Component& GetComponent();
+
+        //template<typename Component>
+        //Component* TryGetComponent();
+
+        //template<typename Component>
+        //bool HasComponent() const;
+
+
+        //template<typename Component, typename... Args>
+        //Component& AddComponent(Args... args);
+
+        //template<typename Component>
+        //Component& AddComponent(Component component);
+
+        //template <typename Component>
+        //Component* AddComponent(GameObject const& src);
+
+
+        //template<typename Component>
+        //void RemoveComponent();
+
+        //template<typename Component>
+        //bool CopyComponent(GameObject const& src);
+        //
+        //template<typename Component>
+        //bool TransferComponent(GameObject const& src);
+
+        ///*template<typename Component>
+        //bool AttemptRemoveComponent();
+
+        //template<typename Component>
+        //Component& AssertComponent();*/
+
+        //template<typename Component, typename... Args>
+        //Component& EnsureComponent(Args... args);
+
+    };
+
+    /*---------------------------------------------------------------------------------*/
+    /* Template Implementation                                                         */
+    /*---------------------------------------------------------------------------------*/
+    //template<typename Component>
+    //Component& GameObject::GetComponent()
+    //{
+    //    return ECS_Manager::GetComponent<Component>(m_entity);
+    //}
+    //
+    //template<typename Component>
+    //Component* GameObject::TryGetComponent()
+    //{
+    //    return ECS_Manager::TryGetComponent<Component>(m_entity);
+    //}
+
+    //template<typename Component>
+    //bool GameObject::HasComponent() const
+    //{
+    //    return ECS_Manager::HasComponent<Component>(m_entity);
+    //}
+
+    //template<typename Component, typename...Args>
+    //Component& GameObject::AddComponent(Args...args)
+    //{
+    //    return ECS_Manager::EmplaceComponent<Component>(m_entity, args...);
+    //}
+
+    //template<typename Component>
+    //Component& GameObject::AddComponent(Component component)
+    //{
+    //    return ECS_Manager::AddComponent<Component>(m_entity);
+    //}
+
+    //template <typename Component>
+    //Component* GameObject::AddComponent(GameObject const& src)
+    //{
+    //    CopyComponent<Component>(src)
+    //    return TryGetComponent<Component>(m_entity);
+    //}
+
+    //template<typename Component>
+    //void GameObject::RemoveComponent()
+    //{
+    //    ECS_Manager::RemoveComponent<Component>(entity.uniqueID);
+    //}
+    //
+    //template<typename Component>
+    //bool GameObject::CopyComponent(GameObject const& src)
+    //{
+    //    return ECS_Manager::CopyComponent<Component>(src.m_entity, m_entity);
+    //}
+
+    //template<typename Component>
+    //bool GameObject::TransferComponent(GameObject const& src)
+    //{
+    //    return ECS_Manager::TransferComponent<Component>(src.m_active, m_entity);
+    //}
+
+    ///*template<typename Component>
+    //bool GameObject::AttemptRemoveComponent()
+    //{
+    //    return ECS_Manager::AttemptRemoveComponent<Component>(entity.uniqueID);
+    //}
+
+    //template<typename Component>
+    //Component& GameObject::AssertComponent()
+    //{
+    //    Component* c = GetComponent<Component>();
+    //    if (c == nullptr)
+    //    {
+    //        std::ostringstream oss;
+    //        oss << "GameObject does not contain a ";
+    //        oss << typeid(Component).name();
+    //        oss << " Component!";
+    //        throw MissingComponentException(oss.str());
+    //    }
+    //    return c;
+    //    ECS_Manager::TryGetComponent
+    //}*/
+
+    //template<typename Component, typename...Args>
+    //Component& GameObject::EnsureComponent(Args...args)
+    //{
+    //    return HasComponent<Component>() ? GetComponent<Component>() : AddComponent<Component>(args...);
+    //}
+
+}
